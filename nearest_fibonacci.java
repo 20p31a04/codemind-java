d5dc9efc5ecdfebef0371f2a37dc62556e40652f
20p31a04 <113285474+20p31a04@users.noreply.github.com>
2022-11-05 06:11:52 +0530

    import java.util.Scanner; class First {
    public static void main(String args[])
    {
     Scanner sc=new Scanner(System.in);
    int n,a=0,b=1,c=0;
    n=sc.nextInt();
    while(c<=n)
    {
    c=a+b;
    a=b;
    b=c;
    }
    if(n-a>b-n)
    System.out.println(b);
    else if(n-a ==b-n)
    System.out.println(a+" "+b);
    else
    System.out.print(a);
    }
    }

---
+++ nearest_fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class First
+{
+
+public static void main(String args[])
+{
+ Scanner sc=new Scanner(System.in);
+int n,a=0,b=1,c=0;
+n=sc.nextInt();
+while(c<=n)
+{
+c=a+b;
+a=b;
+b=c;
+}
+if(n-a>b-n)
+System.out.println(b);
+else if(n-a ==b-n)
+System.out.println(a+" "+b);
+else
+System.out.print(a);
+}
+}
